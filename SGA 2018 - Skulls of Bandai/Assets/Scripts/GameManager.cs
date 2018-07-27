b1d1a6529d7140a7cda137071b3862211ac54fcb
Guillaume <gdvaud@gmail.com>
2018-07-28 01:01:27 +0200

    Progression Bar

--- SGA 2018 - Skulls of Bandai/Assets/Scripts/GameManager.cs
+++ SGA 2018 - Skulls of Bandai/Assets/Scripts/GameManager.cs
@@ -4,36 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    [SerializeField] private int nbItem;
+    public int nbItem { get; set; }
+    public int totalItems { get; set; }
     [SerializeField] private float maxTimerDuration = 60f;
     private float timeSpent;
     private bool isGameEnded;
     private SceneManagerBase sceneManager;
+    [SerializeField] private GameEvent ItemRepairedCountChangedEvent;
+    [SerializeField] private GameEvent OnTimerValueChangedEvent;
 
     // Use this for initialization
     void Start() {
         sceneManager = GetComponent<SceneManagerBase>();
         timeSpent = 0f;
+        nbItem = 0;
+        totalItems = 0;
         isGameEnded = false;
     }
 
     private void Update() {
         timeSpent += Time.deltaTime;
+        OnTimerValueChangedEvent.Fire(new GameEventMessage(this, new Vector2(timeSpent, maxTimerDuration)));
         if (!isGameEnded && timeSpent > maxTimerDuration) {
-            sceneManager.ChangeScene("EndGame");
+            //sceneManager.ChangeScene("EndGame");
             isGameEnded = true;
         }
     }
 
     public void OnItemSpawned(GameEventMessage msg) {
-        nbItem ++;
+        nbItem++;
+        totalItems++;
     }
 
     public void OnItemRepaired(GameEventMessage msg) {
         nbItem--;
-        if (nbItem == 0) {
-            Debug.Log("Ended");
-            sceneManager.ChangeScene("EndGame");
-        }
+        ItemRepairedCountChangedEvent.Fire(new GameEventMessage(this, new Vector2Int(nbItem, totalItems)));
+        //if (nbItem == 0) {
+        //    Debug.Log("Ended");
+        //    sceneManager.ChangeScene("EndGame");
+        //}
     }
 }
---
+++ SGA 2018 - Skulls of Bandai/Assets/Scripts/UI/ProgressionBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressionBar : MonoBehaviour {
+
+    [SerializeField] private RectTransform bob;
+    [SerializeField] private RectTransform car;
+    [SerializeField] private int minPos = 0;
+    [SerializeField] private int maxPos = 1920;
+
+    public void OnItemRepairedChanged(GameEventMessage msg) {
+        Vector2Int values = (Vector2Int)msg.value;
+        float progression = ((float)values.x) / values.y;
+
+        Vector2 pos = bob.anchoredPosition;
+        pos.x = (maxPos - minPos) * progression;
+        bob.anchoredPosition = pos;
+    }
+
+    public void OnTimerValueChanged(GameEventMessage msg) {
+        Vector2 values = (Vector2)msg.value;
+        float progression = values.x / values.y;
+
+        Vector2 pos = car.anchoredPosition;
+        pos.x = (maxPos - minPos) * progression;
+        car.anchoredPosition = pos;
+    }
+}
