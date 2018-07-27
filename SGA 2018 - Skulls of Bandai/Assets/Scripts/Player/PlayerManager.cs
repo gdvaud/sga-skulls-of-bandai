293a293f4709b46a92acce3088514d577e178b4d
Guillaume <gdvaud@gmail.com>
2018-07-27 11:01:24 +0200

    Implementation Player Model

--- SGA 2018 - Skulls of Bandai/Assets/Scripts/Player/PlayerManager.cs
+++ SGA 2018 - Skulls of Bandai/Assets/Scripts/Player/PlayerManager.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour {
 
     [SerializeField] private float movementSpeed = 400f;
+    [SerializeField] private AnimationCurve moveSpeedCurve;
+    [SerializeField] private GameEvent PlayerMovedEvent;
     private Rigidbody rb;
+    private Animator anim;
 
     private void Start() {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
     public void OnMoving(GameEventMessage msg) {
-        Vector3 direction = (Vector3) msg.value;
+        Vector3 direction = (Vector3)msg.value;
+        Vector3 target = transform.position + direction;
+
+        transform.LookAt(target, transform.up);
+
         rb.velocity = direction * movementSpeed * Time.deltaTime;
+
+        anim.SetFloat("MoveSpeed", moveSpeedCurve.Evaluate(direction.magnitude));
+        PlayerMovedEvent.Fire(new GameEventMessage(this, transform.position));
     }
 }
