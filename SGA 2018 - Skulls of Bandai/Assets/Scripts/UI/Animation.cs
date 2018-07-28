e1178f72088ff2fe01d197e150020ea7c9947114
Guillaume <gdvaud@gmail.com>
2018-07-28 10:43:33 +0200

    Lot of things

---
+++ SGA 2018 - Skulls of Bandai/Assets/Scripts/UI/Animation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation : MonoBehaviour {
+
+    [SerializeField] private List<Sprite> sprites;
+    private int index = 0;
+    [SerializeField] float timeBetweenFrames = 0.2f;
+    [SerializeField] private Image image;
+    private float lastFrameChange = 0;
+
+	// Use this for initialization
+	void Start () {
+        index = 0;
+        image.sprite = sprites[index];
+        lastFrameChange = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastFrameChange + timeBetweenFrames < Time.time) {
+            index++;
+            if (index >= sprites.Count) {
+                index = 0;
+            }
+
+            lastFrameChange = Time.time;
+            image.sprite = sprites[index];
+        }
+	}
+}
---
+++ SGA 2018 - Skulls of Bandai/Assets/Scripts/UI/EndGameMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndGameMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject win;
+    [SerializeField] private GameObject lose;
+    private GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+        gm = FindObjectOfType<GameManager>();
+        if (gm.nbItem == 0) {
+            win.SetActive(true);
+            lose.SetActive(false);
+        } else {
+            win.SetActive(false);
+            lose.SetActive(true);
+        }
+    }
+}
